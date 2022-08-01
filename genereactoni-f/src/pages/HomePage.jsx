625cddd4187cacd6a55a8c2176f4dce0e9feb22e
FickosOP <filip.kresa@gmail.com>
2022-08-01 04:35:41 +0200

    Pan and zoom in combination with drag and drop

--- genereactoni-f/src/pages/HomePage.jsx
+++ genereactoni-f/src/pages/HomePage.jsx
@@ -3,10 +3,13 @@ import Sidebar from "../components/Sidebar";
 import Picture from "../components/Picture";
 import { useState } from "react";
 import { useDrop } from "react-dnd";
+import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 
 function HomePage(){
 
     const [canvas, setCanvas] = useState([]);
+
+    const [isMoving, setMoving] = useState(false);
 
     const [, drop] = useDrop(() => ({
         accept: "image",
@@ -29,22 +32,48 @@ function HomePage(){
         setCanvas(nc);
     }
     //useEffect
-
     return(
     
     <div className="App">
       <Header active="home" />
       <Sidebar />
-        <div className="canvas" ref={drop} style={{height:"500px"}}>
+      <button style={{position:"relative", zIndex:"1", backgroundColor: "#fff"}} onClick={() => setMoving(!isMoving)}>KAD JA KAEM</button>
+      {!isMoving && <TransformWrapper
+        initialScale={1}
+        minScale={.5}
+        maxScale={2}
+        limitToBounds={false}
+        pinch={{step:5}}
+      >
+      <TransformComponent
+        contentStyle={{ height: '100%', width: '100%', position:'relative', backgroundColor: '#5e687a'}}
+        wrapperStyle={{ height: '100%', width: '100%', position:'absolute', left: '260px', top: '59px', backgroundColor: '#5e687a'}}
+      >
+        <div className="canvas" ref={drop}>
             {
                 canvas.map((picture) => {
                     // console.log(`W:${picture.x}\nH:${picture.y}`);
                     // return <div style={{position:"absolute", top:`${picture.y}px`, left: `${picture.x}px`}}>dd</div>; //Instead of using picture use
-                    return <Picture url={picture.url} id={picture.id} inCanvas={true} left={picture.x} top={picture.y} key={picture.id} />;
+                    return <Picture url={picture.url} id={picture.id} inCanvas={true} left={picture.x - 260} top={picture.y - 59} key={picture.id} />;
                 })
             }
         </div>
+      </TransformComponent>
+      </TransformWrapper>
+    }
+    {isMoving && 
+        <div className="canvas" ref={drop}>
+            {
+                canvas.map((picture) => {
+                    // console.log(`W:${picture.x}\nH:${picture.y}`);
+                    // return <div style={{position:"absolute", top:`${picture.y}px`, left: `${picture.x}px`}}>dd</div>; //Instead of using picture use
+                    return <Picture url={picture.url} id={picture.id} inCanvas={true} left={picture.x - 260} top={picture.y - 59} key={picture.id} />;
+                })
+            }
+        </div>
+    }
     </div>
+    
     )
 }
 
